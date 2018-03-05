1d8e04947c438564841a9ed45a47d5361129ec0e
Step7750 <Step7750@gmail.com>
2018-03-05 12:30:38 -0700

    MemePhase -> MemePlace App Title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,6 +20,6 @@ describe('AppComponent', () => {
   it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('MemePhase');
+    expect(app.title).toEqual('MemePlace');
   }));
 });
