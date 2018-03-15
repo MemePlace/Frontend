fc2e1e73710e703b9bbde77ae3325baed284ceb6
JVLelody <xiaoxining@hotmail.com>
2018-03-15 14:51:02 -0600

    creation component created

--- src/app/community/community.module.ts
+++ src/app/community/community.module.ts
@@ -4,14 +4,17 @@ import { BrowseComponent } from './browse/browse.component';
 import { MemeCardComponent } from '../meme-card/meme-card.component';
 import { MatTabsModule, MatIconModule } from '@angular/material';
 import { MatCardModule } from '@angular/material';
+import { CreationComponent } from './creation/creation.component';
+import { MatStepperModule } from '@angular/material/stepper';
 
 @NgModule({
   imports: [
     CommonModule,
     MatTabsModule,
     MatCardModule,
-    MatIconModule
+    MatIconModule,
+    MatStepperModule,
   ],
-  declarations: [BrowseComponent, MemeCardComponent]
+  declarations: [BrowseComponent, MemeCardComponent, CreationComponent]
 })
 export class CommunityModule { }
