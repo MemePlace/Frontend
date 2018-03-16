4e61d9b24d876b662341297da5ae15f9599639e7
xiningchen <xiaoxining@hotmail.com>
2018-03-15 19:53:44 -0600

    removes comments

--- src/app/community/community.module.ts
+++ src/app/community/community.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowseComponent } from './browse/browse.component';
 import { MemeCardComponent } from '../meme-card/meme-card.component';
-import { MatTabsModule, MatIconModule } from '@angular/material';
-import { MatCardModule } from '@angular/material';
+import { MatTabsModule, MatIconModule, MatCardModule, MatButtonModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     MatTabsModule,
     MatCardModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule
   ],
   declarations: [BrowseComponent, MemeCardComponent]
 })
