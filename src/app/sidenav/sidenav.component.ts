e970025f0171aca860c9ba610043234b03e6615d
DavidS <sdave0921@gmail.com>
2018-03-16 00:43:26 -0600

    Adds Favorite community icon

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -8,6 +8,9 @@ import {MatSidenav} from '@angular/material';
 })
 export class SidenavComponent implements OnInit {
   @ViewChild('sidenav') sidenav: MatSidenav;
+  isBookmarked = false;
+  communityName: String;
+  communities: Array<any>;
 
   get screenWidth(): number {
     return window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
@@ -24,9 +27,21 @@ export class SidenavComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.communities = new Array();
+    this.communities = [
+      { communityname: this.communityName = 'Community 1', isbookmarked: this.isBookmarked },
+      { communityname: this.communityName = 'Community 2', isbookmarked: this.isBookmarked }
+    ];
   }
 
   toggle() {
     this.sidenav.toggle();
+  }
+
+  bookmark(i: any) {
+    this.communities[i].isbookmarked = true;
+  }
+  removeBookmark(i: any) {
+    this.communities[i].isbookmarked = false;
   }
 }
