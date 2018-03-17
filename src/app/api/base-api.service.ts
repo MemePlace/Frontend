1f376c45541542fd4689e98ff4fe2fa5e3f4d78d
Step7750 <Step7750@gmail.com>
2018-03-17 01:09:52 -0600

    Configures Different API Base URL based upon whether dev mode

--- src/app/api/base-api.service.ts
+++ src/app/api/base-api.service.ts
@@ -1,28 +1,33 @@
-import { Injectable } from '@angular/core';
+import { Injectable, isDevMode } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 export enum Version {
   v1 = 'v1'
 }
 
+const prodBase = 'api.meme.place';
+const devBase = '';
+
 @Injectable()
 export class BaseApiService {
+  base = isDevMode() ? devBase : prodBase;
 
   constructor(private http: HttpClient) { }
 
   get(version: Version, resource: string) {
-    return this.http.get(`/${version}/${resource}`);
+    console.log(resource);
+    return this.http.get(`${base}/${version}/${resource}`);
   }
 
   post(version: Version, resource: string, data: {}) {
-    return this.http.post(`/${version}/${resource}`, data);
+    return this.http.post(`${base}/${version}/${resource}`, data);
   }
 
   put(version: Version, resource: string, data: {} | null) {
-    return this.http.put(`/${version}/${resource}`, data);
+    return this.http.put(`${base}/${version}/${resource}`, data);
   }
 
   delete(version: Version, resource: string) {
-    return this.http.delete(`/${version}/${resource}`);
+    return this.http.delete(`${base}/${version}/${resource}`);
   }
 }
