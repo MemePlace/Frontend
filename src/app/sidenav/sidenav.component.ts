b0b7f08d388a5cf99e81aa3b6840025ba3263aa9
DavidS <sdave0921@gmail.com>
2018-03-21 22:36:17 -0600

    Adds favourited communities list

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -30,15 +30,20 @@ export class SidenavComponent implements OnInit {
   ngOnInit() {
     this.communities = new Array();
     this.communitiesFavorited = new Array();
+    // populate 20 random communities
     for (let i = 0; i < 20; i++) {
-      this.communities.push({ communityname: this.communityName = 'Community ' + i, isFavorited: this.isFavorited });
+      this.communities.push({ communityname: this.communityName = 'Community ' + i, 
+      isFavorited: this.isFavorited, 
+      favouriteCount: Math.floor(Math.random()*(101))});
     }
     for (let community of this.communities) {
       if (community.isFavorited === true) this.communitiesFavorited.push(community);
     }
+    this.communities.sort((a, b) => b.favouriteCount - a.favouriteCount);
     this.communitiesFavorited.sort((a, b) => a.communityname.localeCompare(b.communityname));
+    console.log(this.communities);
   }
-
+  
   toggle() {
     this.sidenav.toggle();
   }
@@ -46,10 +51,17 @@ export class SidenavComponent implements OnInit {
   favorite(i: any) {
     this.communities[i].isFavorited = true;
     this.communitiesFavorited.push(this.communities[i]);
+    this.communitiesFavorited.sort((a, b) => a.communityname.localeCompare(b.communityname));
   }
   removeFavorite(i: any) {
     this.communities[i].isFavorited = false;
     let index = this.communitiesFavorited.indexOf(this.communities[i]);
     if (index !== -1) this.communitiesFavorited.splice(index, 1);
+  }
+  removeFavoriteFromFav(i: any) {
+    this.communitiesFavorited[i].isFavorited = false;
+    let index = this.communities.indexOf(this.communitiesFavorited[i]);
+    if (index !== -1) this.communities.splice(index, 1);
+    this.communitiesFavorited.splice(i, 1);
   }
 }
