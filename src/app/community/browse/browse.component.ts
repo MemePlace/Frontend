33cdf1cff1bde2a516b9e1a5ba60a6e7b18c3779
Dante Bencivenga <drbenciv@ucalgary.ca>
2018-03-26 14:55:26 -0600

    Begins adding a responsive meme-card sizing system for mobile

--- src/app/community/browse/browse.component.ts
+++ src/app/community/browse/browse.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-browse',
@@ -15,8 +15,9 @@ export class BrowseComponent implements OnInit {
   user4 = {un: 'clayton123', height: '300', img: 'https://fthmb.tqn.com/onZS-nRlttC_o-4JSQEXImdfL3E=/768x0/filters:no_upscale()/success-56a9fd1f3df78cf772abee09.jpg'};
   user5 = {un: 'dave--2', height: '300', img: 'http://i0.kym-cdn.com/entries/icons/original/000/021/353/6ec4b66c67510dc2734cf478fc033bdc.jpg'};
   user6 = {un: 'sam10101', height: '300', img: 'https://i.ytimg.com/vi/XAQmlzsmOY4/maxresdefault.jpg'};
+  user7 = {un: 'dante888', height: '723', img: 'http://i0.kym-cdn.com/photos/images/original/001/224/694/300'};
 
-  users = [this.user1, this.user2, this.user3, this.user4, this.user5, this.user6];
+  users = [this.user1, this.user2, this.user3, this.user4, this.user5, this.user6, this.user7];
 
   ngOnInit() {
   }
