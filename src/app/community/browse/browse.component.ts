22336ff5bd5d9d1e84b34a42dc655423103511ef
Dante Bencivenga <drbenciv@ucalgary.ca>
2018-03-27 17:16:23 -0600

    finishes mobile width-fixing and renames "angular-project" to "Meme.Place"

--- src/app/community/browse/browse.component.ts
+++ src/app/community/browse/browse.component.ts
@@ -15,11 +15,20 @@ export class BrowseComponent implements OnInit {
   user4 = {un: 'clayton123', height: '300', img: 'https://fthmb.tqn.com/onZS-nRlttC_o-4JSQEXImdfL3E=/768x0/filters:no_upscale()/success-56a9fd1f3df78cf772abee09.jpg'};
   user5 = {un: 'dave--2', height: '300', img: 'http://i0.kym-cdn.com/entries/icons/original/000/021/353/6ec4b66c67510dc2734cf478fc033bdc.jpg'};
   user6 = {un: 'sam10101', height: '300', img: 'https://i.ytimg.com/vi/XAQmlzsmOY4/maxresdefault.jpg'};
-  user7 = {un: 'dante888', height: '723', img: 'http://i0.kym-cdn.com/photos/images/original/001/224/694/300'};
+  user7 = {un: 'dante888', height: '300', img: 'https://imgs.xkcd.com/comics/exploits_of_a_mom.png'};
+  user8 = {un: 'dante888', height: '300', img: 'http://i0.kym-cdn.com/photos/images/original/001/224/694/300'};
 
-  users = [this.user1, this.user2, this.user3, this.user4, this.user5, this.user6, this.user7];
+  users = [this.user1, this.user2, this.user3, this.user4, this.user5, this.user6, this.user7, this.user8];
 
   ngOnInit() {
+  }
+
+  get screenWidth(): number {
+    return window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  }
+
+  get isMobile(): boolean {
+    return this.screenWidth <= 1000;
   }
 
 }
--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -9,10 +9,12 @@ import {MatSidenav} from '@angular/material';
 export class SidenavComponent implements OnInit {
   @ViewChild('sidenav') sidenav: MatSidenav;
 
+  // I would like screenWidth and isMobile to be accessible from multiple components but don't know how
   get screenWidth(): number {
     return window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
   }
 
+  // I'm not sure if 1000px is a great cutoff point - seems a bit too big (maybe decrease to ~768px?)
   get isMobile(): boolean {
     return this.screenWidth <= 1000;
   }
