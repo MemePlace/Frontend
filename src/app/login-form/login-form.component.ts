4238a959b72fc16b7389cc218126fae9e7329dd4
Niu <bankaipwn@gmail.com>
2018-04-02 13:55:57 -0600

    Connected UserService
    Used DI to connect UserService to login/register page
    
    To do: Deal with connecting the information to backend

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import { UserService } from '../api/user.service';
 
 @Component({
   selector: 'app-login-form',
@@ -10,13 +11,25 @@ import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 export class LoginFormComponent implements OnInit {
 
     constructor(public dialogRef: MatDialogRef<LoginFormComponent>,
+                private userService: UserService,
                 public dialog: MatDialog) { }
 
+    usernameLogin: string;
+    passwordLogin: string;
+    email: string;                
+                
     ngOnInit() {
     }
 
     loginValidate(){
         // Validate user login
+        this.userService.login(this.usernameLogin, this.passwordLogin).then((user) => {
+//           this.users = user;
+            this.dialogRef.close();      
+        }).catch((err) =>{
+            console.error(err);
+        });
+        
     }
   
     registerPage(){
@@ -38,14 +51,27 @@ export class LoginFormComponent implements OnInit {
 })
 export class LoginFormRegister {
     constructor(public dialogRef: MatDialogRef<LoginFormRegister>,
+                private userService: UserService,
                 @Inject(MAT_DIALOG_DATA) public data: any) {}
                 
+    usernameRegister: string;
+    passwordRegister: string;
+    email: string;
+//    users: 
+    
     registerValidate(){
-        // Validate user registration
+        // Validate user registration by calling signup from UserService
+        this.userService.signup(this.usernameRegister, this.passwordRegister, this.email).then((user) => {
+//           this.users = user;
+            this.dialogRef.close();
+        }).catch((err) =>{
+            console.error(err);
+        });
+        
     }
                 
     cancel(): void{
-        // Go to Login page from register page
+        // Go to Login/Main page from register page
         this.dialogRef.close();
     }
 }
