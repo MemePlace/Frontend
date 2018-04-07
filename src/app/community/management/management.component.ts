fdc0e31277544ad880312bef9e9371d40c9ed8cc
xiningchen <xiaoxining@hotmail.com>
2018-04-07 01:47:20 -0600

    basic input fields

--- src/app/community/management/management.component.ts
+++ src/app/community/management/management.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./management.component.scss']
 })
 export class ManagementComponent implements OnInit {
+  titleText: string;
+  descriptionText: string;
+  sidebarText: string;
+  NSFW_checked: boolean;
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  onUpdateCommunity() {
+
+  }
+
 
 }
