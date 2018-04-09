c72c4204b47392978d3aefb1ee70f465626fe695
xiningchen <xiaoxining@hotmail.com>
2018-04-09 00:05:22 -0600

    cleans up list of users

--- src/app/community/browse/browse.component.ts
+++ src/app/community/browse/browse.component.ts
@@ -12,17 +12,16 @@ export class BrowseComponent implements OnInit {
 
   constructor() { }
 
-  user1 = {un: 'shining777', height: '300', memeId: 8};
-  user2 = {un: 'dante888', height: '300', memeId: 9};
-  user3 = {un: 'step7750', height: '300', memeId: 10};
-  user4 = {un: 'clayton123', height: '300', memeId: 11};
-  user5 = {un: 'dave--2', height: '300', memeId: 12};
-  user6 = {un: 'sam10101', height: '300', memeId: 13};
-  user7 = {un: 'dante888', height: '300', memeId: 14};
-  user8 = {un: 'dante888', height: '300', memeId: 15};
+  user1 = {height: '300', memeId: 8};
+  user2 = {height: '300', memeId: 9};
+  user3 = {height: '300', memeId: 10};
+  user4 = {height: '300', memeId: 11};
+  user5 = {height: '300', memeId: 12};
+  user6 = {height: '300', memeId: 13};
+  user7 = {height: '300', memeId: 14};
+  user8 = {height: '300', memeId: 15};
 
   users = [this.user1, this.user2, this.user3, this.user4, this.user5, this.user6, this.user7, this.user8];
-  // users = [this.user1];
 
   ngOnInit() {
   }
