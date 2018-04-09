84a16bf1c534c85452efe24ac9f057df5176cd8e
Step7750 <Step7750@gmail.com>
2018-04-09 16:28:39 -0600

    Adds Async Community Validator For Checking if Name is Taken

--- src/app/community/create/create.component.ts
+++ src/app/community/create/create.component.ts
@@ -1,29 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {CommunityService} from '../../api/community.service';
 
 @Component({
   selector: 'app-community-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.scss']
 })
-export class CreateComponent implements OnInit {
-  nameFormControl = new FormControl('', [
-    Validators.required,
-  ]);
-
-  titleFormControl = new FormControl('', [
-    Validators.required,
-  ]);
-
+export class CreateComponent {
+  private nameTimeout;
   form: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private communityService: CommunityService) {
     this.createForm();
   }
 
   createForm() {
     this.form = this.fb.group({
-      name: ['', [Validators.required, Validators.minLength(3)]],
+      name: ['', [Validators.required, Validators.minLength(3)], [this.nameExists.bind(this)]],
       title: ['', Validators.required],
       description: [''],
       sidebar: [''],
@@ -32,7 +26,20 @@ export class CreateComponent implements OnInit {
     console.log(this.form);
   }
 
-  ngOnInit() {
+  nameExists(control: FormControl) {
+    clearTimeout(this.nameTimeout);
+
+    return new Promise((resolve, reject) => {
+      this.nameTimeout = setTimeout(() => {
+        this.communityService.isCommunityNameAvailable(control.value).then((exists) => {
+          if (exists) {
+            resolve({nameExists: true});
+          } else {
+            resolve(null);
+          }
+        }).catch((err) => resolve(null));
+      }, 250);
+    })
   }
 
   onCreateCommunity() {
