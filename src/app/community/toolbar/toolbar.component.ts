049137168fc161c38d374bea58207f4006ed818c
Step7750 <Step7750@gmail.com>
2018-04-11 19:07:48 -0600

    Implements Favouriting Functionality

--- src/app/community/toolbar/toolbar.component.ts
+++ src/app/community/toolbar/toolbar.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Community, CommunityService} from '../../api/community.service';
+import {MatSnackBar} from '@angular/material';
 
 @Component({
   selector: 'app-community-toolbar',
@@ -9,7 +10,50 @@ import {Community, CommunityService} from '../../api/community.service';
 export class ToolbarComponent implements OnInit {
   @Input() community: Community;
 
-  ngOnInit() {
+  get isFavourited() {
+    return this.communityService.isFavourited(this.community.name);
   }
 
+  favourites_ = 0;
+
+  set totalFavourites(favourites: number) {
+    this.favourites_ = favourites;
+  }
+
+  get totalFavourites() {
+    let v = this.favourites_;
+
+    if (this.isFavourited) {
+      v += 1;
+    }
+
+    return v;
+  }
+
+  constructor(private communityService: CommunityService,
+              private snackBar: MatSnackBar) { }
+
+  ngOnInit() {
+    let fav = this.community.favourites;
+
+    if (this.isFavourited) {
+      fav -= 1;
+    }
+
+    this.totalFavourites = fav;
+  }
+
+  async toggleFavourite() {
+    const isFavourited = this.communityService.isFavourited(this.community.name);
+
+    try {
+      const promise = (isFavourited) ? this.communityService.unfavourite(this.community) :
+        this.communityService.favourite(this.community);
+
+      await promise;
+    } catch(err) {
+      this.snackBar.open(`Failed to favourite: ${err.message}`, 'Close');
+    }
+
+  }
 }
