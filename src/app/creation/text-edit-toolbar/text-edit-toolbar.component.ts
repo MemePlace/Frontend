ef5cfa82f7920d86f50e3247443fb230e68708d1
Step7750 <Step7750@gmail.com>
2018-04-19 17:41:50 -0600

    Fixes Y Offset When Zooming

--- src/app/creation/text-edit-toolbar/text-edit-toolbar.component.ts
+++ src/app/creation/text-edit-toolbar/text-edit-toolbar.component.ts
@@ -33,8 +33,8 @@ export class TextEditToolbarComponent implements OnInit {
 
       console.log('editing entered');
       console.log(this.canvas.getActiveObject());
-      this.bottom = this.canvas.height - object.top + 20;
-      this.left = object.left + (object.width / 2);
+      this.bottom = (this.canvas.height - object.top * object.zoomY) + 20;
+      this.left = (object.left + (object.width / 2)) * object.zoomX;
       this.hidden = false;
     });
 
